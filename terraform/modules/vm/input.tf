5e6af20f6a3f5905f39a3c34b310f87299c206d9
nil-github <66524063+nil-github@users.noreply.github.com>
2024-03-26 19:08:04 +0530

    Update input.tf

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,1 +1,13 @@
 
+variable "vm_admin_username" {
+    default = "adminuser"
+}
+variable "vm_size" {
+    default = "Standard_B1s"
+}
+variable "application_type" {}
+variable "resource_type" {}
+variable "location" {}
+variable "subnet_id" {}
+variable "resource_group" {}
+variable "public_ip_address_id" {}
