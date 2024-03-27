9d7c28b35bb605b114a0ecbe58e72e372b8d2ca6
nil-github <66524063+nil-github@users.noreply.github.com>
2024-03-27 13:16:24 +0530

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -7,10 +7,10 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
-    access_key           = ""
+    storage_account_name = "tfstate137351017"
+    container_name       = "tfstate"
+    key                  = "test.terraform.tfstate"
+    access_key           = "Hmc+48dGU/cVB3nj8Sdh6FiZpECRCJrd/4MwrSQ2DH5f64IPlYl+78gQK9r+DkZrtUQFzfHrldrg+AStcHKq3Q=="
   }
 }
 module "resource_group" {
