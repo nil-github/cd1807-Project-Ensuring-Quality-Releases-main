77b6bc01e2784386578b0bb86e1f743bd23e5c62
nil-github <66524063+nil-github@users.noreply.github.com>
2024-04-02 17:54:17 +0530

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,8 @@ resource "azurerm_linux_virtual_machine" "test" {
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
     username   = "adminuser"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTbt9HS8LajW8oWM19PkUzYMEK59XJ7otp1UwT3r4jat1m9LVQ+3xuLLMBt6+sIIkbnxlZDUO8XxL3cWAi/5UnHdyYIXLdlNInrbYKltHvOJnk8eFmKPVqtwQS45J7giAd5g587q6hkOwrDmPJ5FebhHuBeiJXWj5RYGbqGxuU1ThMfR4us1WeKs3yxN3nvT72eoKS7oM6cMWURghJUjihtnx/0hRigcfGuzF6OXcj8dpa63WKeL4+CtoI4Jq4tAGGZZ9Jkx1alTohFhpfRwLB0ZG51gcn8DWg+mkL6HTrkToLXK02088Ibr/vPr7GGuo7eSGSLE4cYTqy8grgm2js5NmjyvjHt/Z2eIw+32sS41t+E0GUwrWjts0FgdYb1Nrp/Y9Sony68nxRKfRdjZT5vnb82T2GpaiKhZgkEC+U8B+wVr8nScgd2p1p9WyhdvqM44cg/CRJUjZQhsdhk4JPrgPwJTJR6ovnezYfFUdLMU7jtHb7F5nqpkmYnMCjcxE= Anil.Bhat@BNGEST-L-99705"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDwKIcalZujy9X1pKZcqUCfah+Dv6ObWaLJBv224D1jw5i9WKKHN8r/yIH1z0pNIpRZEgzbVdtS4x1iObNm4rJSqCe7z9KQQ/aZYgPXf0oY6XaQavl7GMOkgmH5waQ05ukYOvMCBmBsYxaKdkIkN7wFyIMlSFz9jSJkjUocEfZvc6dDlAavthi69cEFUbbetUb8r4gD5NolT2nZFb05cq2tcZjitR3OK1+Y+wLhep5bancom5MOtcJ2Qj0wawMAeexCOOCvUwJ+e14tHMm8AiAi4OfUYIQ4X6d2hWI0WDgs+uyK8mQMx4v+AQpVcsY2v2mc9zTS9C71tfzxEeKfA0uLIAItXY+zG8me7j4ouVWUrwoO/1pWB3V1AVEnYThT8Nkz0jWptThDuYIrq+9SrKdgbVr6BFCWA+EfsjM+rh2DcGwG95N4NzQ6iufMchYGHSXhMy+SE7wNHEpDUW8HJCL6z/cplnLa2I90OJEtugq5uzOIoAmG3ZowpPxIFvAtD2U= Anil.Bhat@BNGEST-L-99705
+"
   }
   os_disk {
     caching           = "ReadWrite"
